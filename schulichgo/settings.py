bfde0138699f9771cbcadcfee08ccd6f620bf110
syedaminx <amin.syed@outlook.com>
2019-10-03 02:32:35 -0400

    initial deployment settings

--- schulichgo/settings.py
+++ schulichgo/settings.py
@@ -17,10 +17,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', 'ec2-18-217-227-247.us-east-2.compute.amazonaws.com']
 
 # Application definition
 
